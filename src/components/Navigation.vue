07c30f79bd3214916d5857da0bb592af4b7114ed
Felix <felixr91@gmail.com>
2019-03-29 17:27:11 -0600

    initproject

---
+++ src/components/Navigation.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="">
+    <div class="row">
+      <div class="col-12">
+        <h1>Freephonics.org</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-2">
+        <span>Alphabet</span>
+      </div>
+      <div class="col-2">
+        <span>Digraphs</span>
+      </div>
+      <div class="col-2">
+        <span>Trigraphs</span>
+      </div>
+      <div class="col-2">
+        <span>Blends</span>
+      </div>
+      <div class="col-2">
+        <span>Dipthongs</span>
+      </div>
+      <div class="col-2">
+        <span>Vowel Pairs</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Navigation',
+    data() {
+      return {
+
+      }
+    },
+    computed: {},
+    methods: {}
+  }
+
+</script>
+
+<style>
+
+
+</style>
